75c3d8f0432ba44e5d337ef4a0f84e0e74ba09f2
Haye <thomas@tmq.be>
2023-10-24 13:50:43 +0200

    navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,71 @@
+library sweet_nav_bar;
+
 import 'package:flutter/material.dart';
 
+import './widgets/sweet_nav_bar.dart';
+
 void main() {
-  runApp(
-    const Center(
-      child: Text(
-        "Hello Flutter!",
-        textDirection: TextDirection.ltr,
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SweetNavBar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    ),
-  );
+      home: const SweetNaveBar(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SweetNaveBar extends StatefulWidget {
+  const SweetNaveBar({Key? key}) : super(key: key);
+
+  @override
+  State<SweetNaveBar> createState() => _SweetNavBarState();
+}
+
+class _SweetNavBarState extends State<SweetNaveBar> {
+  final List<Widget> _items = [
+    const Text('Home'),
+    const Text('Business'),
+    const Text('School'),
+  ];
+
+  int cIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+      bottomNavigationBar: SweetNavBar(
+        currentIndex: cIndex,
+        paddingBackgroundColor: Colors.transparent,
+        items: [
+          SweetNavBarItem(
+              sweetActive: const Icon(Icons.home),
+              sweetIcon: const Icon(
+                Icons.home_outlined,
+              ),
+              sweetLabel: 'Home',
+              sweetBackground: Colors.red),
+          SweetNavBarItem(
+              sweetIcon: const Icon(Icons.business), sweetLabel: 'Business'),
+          SweetNavBarItem(
+              sweetIcon: const Icon(Icons.school), sweetLabel: 'School'),
+        ],
+        onTap: (index) {
+          setState(() {
+            cIndex = index;
+          });
+        },
+      ),
+    );
+  }
 }
