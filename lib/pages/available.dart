f0f0a32e3145d3ccd130c870690ce48296f219c4
Senne Vansant <r0894963@student.thomasmore.be>
2023-11-01 10:30:38 +0100

    api gefixt

--- lib/pages/available.dart
+++ lib/pages/available.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import '../models/equations.dart';
+import '../apis/equations_api.dart';
 
-class AvailableScreen extends StatelessWidget {
+class AvailableScreen extends StatefulWidget {
   const AvailableScreen({Key? key}) : super(key: key);
 
   @override
+  State<StatefulWidget> createState() => _AvailableScreenState();
+}
+
+class _AvailableScreenState extends State {
+  List<User> userList = [];
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  void _getUsers() {
+    EquationsApi.fetchUsers().then((result) {
+      setState(() {
+        userList = result;
+        count = result.length;
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Available Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Users"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(5.0),
+        child: _userListItems(),
+      ),
     );
   }
+
+  ListView _userListItems() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: Text(userList[position].lastname),
+        );
+      },
+    );
+  }
+  
 }
